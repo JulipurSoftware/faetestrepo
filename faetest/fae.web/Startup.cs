bdbee0451a29bba07da1a4c2fa651220dce83a02
Craig Gleadall <gleadall@hotmail.com>
2015-06-16 16:12:48 -0400

    initial web for Feature101

---
+++ faetest/fae.web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(fae.web.Startup))]
+namespace fae.web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
